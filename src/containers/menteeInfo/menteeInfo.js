646094a9e4220e4e60a97bc0c9c2c72a017d2ee7
kim nan seul <154604350+seulnan@users.noreply.github.com>
2024-12-07 23:09:07 +0900

    [FIX] 리팩토링중 map cannot read오류
    컴포넌트는 항상대문자로
    menteeInfoCard가 아니라 menteeInfo를 map 해야함

--- src/containers/menteeInfo/menteeInfo.js
+++ src/containers/menteeInfo/menteeInfo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./menteeInfo.scss";
-import menteeInfoCard from "../../components/menteeInfoCard/menteeInfoCard";
+import MenteeInfoCard from "../../components/menteeInfoCard/menteeInfoCard";
 import { menteeInfo } from "../../portfolio";
 
 export default function MenteeInfo() {
@@ -9,12 +9,12 @@ export default function MenteeInfo() {
       <div className="menteeInfo-section" id="menteeInfo">
         <h1 className="menteeInfo-heading">introduce</h1>
         <div className="menteeInfo-card-container">
-          {menteeInfoCard.schools.map((school, index) => (
-            <menteeInfoCard key={index} school={school} />
+          {menteeInfo.schools.map((school, index) => (
+            <MenteeInfoCard key={index} school={school} />
           ))}
         </div>
       </div>
-    );
+    );  
   }
   return null;
 }
